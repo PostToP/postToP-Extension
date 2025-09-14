2f5801c122f951f641cc017a57e49106452477cd
Pataki Dávid Ferenc <pataki.david.ferenc@gmail.com>
2025-09-14 16:40:34 +0200

    refactor: consolidate webpack configuration and remove unused files

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -3,102 +3,77 @@ const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const srcDir = path.join(__dirname, "..", "src");
 
-module.exports = {
-  entry: {
-    background: path.join(srcDir, "background/index.ts"),
-    content_script: path.join(srcDir, "script/index.ts"),
-    popup: path.join(srcDir, "popup/index.tsx"),
-    settings: path.join(srcDir, "settings/index.tsx"),
-  },
-  output: {
-    path: path.join(__dirname, "../dist/js"),
-    filename: "[name].js",
-  },
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
 
-  optimization: {
-    splitChunks: {
-      name: "vendor",
-      chunks(chunk) {
-        return chunk.name !== "background";
-      },
-      cacheGroups: {
-        preact: {
-          test: /[\\/]node_modules[\\/]preact/,
-          name: "preact",
-          chunks: "all",
-          enforce: true,
+  return {
+    entry: {
+      background: path.join(srcDir, "background/index.ts"),
+      content_script: path.join(srcDir, "script/index.ts"),
+      popup: path.join(srcDir, "popup/index.tsx"),
+      settings: path.join(srcDir, "settings/index.tsx"),
+    },
+    output: {
+      path: path.join(__dirname, "../dist/js"),
+      filename: "[name].js",
+      clean: true,
+    },
+    mode: isProduction ? "production" : "development",
+    devtool: isProduction ? false : "inline-source-map",
+
+    optimization: {
+      splitChunks: !isProduction
+        ? false
+        : {
+            name: "vendor",
+            chunks(chunk) {
+              return chunk.name !== "background";
+            },
+            cacheGroups: {
+              preact: {
+                test: /[\\/]node_modules[\\/]preact/,
+                name: "preact",
+                chunks: "all",
+                enforce: true,
+              },
+            },
+          },
+      minimize: isProduction,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "ts-loader",
+              options: {
+                configFile: path.resolve(__dirname, "../tsconfig.json"),
+                compilerOptions: {
+                  jsx: "react-jsx",
+                  jsxImportSource: "preact",
+                },
+                transpileOnly: !isProduction,
+              },
+            },
+          ],
         },
+      ],
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+      alias: {
+        react: "preact/compat",
+        "react-dom": "preact/compat",
+        "react/jsx-runtime": "preact/jsx-runtime",
       },
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        include: path.resolve(__dirname, "../src/background"),
-        use: {
-          loader: "ts-loader",
-          options: {
-            configFile: path.resolve(
-              __dirname,
-              "../src/background/tsconfig.json"
-            ),
-          },
-        },
-      },
-      {
-        test: /\.tsx?$/,
-        include: path.resolve(__dirname, "../src/popup"),
-        use: {
-          loader: "ts-loader",
-          options: {
-            configFile: path.resolve(__dirname, "../src/popup/tsconfig.json"),
-          },
-        },
-      },
-      {
-        test: /\.tsx?$/,
-        include: path.resolve(__dirname, "../src/script"),
-        use: {
-          loader: "ts-loader",
-          options: {
-            configFile: path.resolve(__dirname, "../src/script/tsconfig.json"),
-          },
-        },
-      },
-      {
-        test: /\.tsx?$/,
-        include: path.resolve(__dirname, "../src/settings"),
-        use: {
-          loader: "ts-loader",
-          options: {
-            configFile: path.resolve(
-              __dirname,
-              "../src/settings/tsconfig.json"
-            ),
-          },
-        },
-      },
-      {
-        test: /\.tsx?$/,
-        include: path.resolve(__dirname, "../src/common"),
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
+    plugins: [
+      new CopyPlugin({
+        patterns: [{ from: ".", to: "../", context: "public" }],
+        options: {},
+      }),
     ],
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-    alias: {
-      react: "preact/compat",
-      "react-dom": "preact/compat",
-      "react/jsx-runtime": "preact/jsx-runtime",
-    },
-  },
-  plugins: [
-    new CopyPlugin({
-      patterns: [{ from: ".", to: "../", context: "public" }],
-      options: {},
-    }),
-  ],
+  };
 };
