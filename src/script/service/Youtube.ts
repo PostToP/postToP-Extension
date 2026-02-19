7f47ac810e0dc073d4bc09255dcbbc34970e07fe
GitDevla <pataki.david.ferenc@gmail.com>
2026-02-19 20:34:49 +0100

    fix: improve getWatchID method to handle miniplayer

--- src/script/service/Youtube.ts
+++ src/script/service/Youtube.ts
@@ -1,4 +1,4 @@
-import {MusicService} from "./MusicService";
+import { MusicService } from "./MusicService";
 
 export default class Youtube extends MusicService {
   public static async pullData() {
@@ -11,6 +11,15 @@ export default class Youtube extends MusicService {
   }
 
   private static async getWatchID() {
-    return Youtube.getWatchIDURL();
+    return Youtube.getWatchIDURL() ?? Youtube.getWatchIDMiniplayer() ?? "Unknown";
+  }
+
+  private static getWatchIDMiniplayer() {
+    const miniplayer = document.querySelector<HTMLAnchorElement>('ytd-playlist-panel-video-renderer[selected]  ytd-thumbnail>a')
+    if (miniplayer) {
+      const miniplayerHref = miniplayer.href;
+      const miniplayerWatchID = miniplayerHref.match(/v=([^&#]{5,})/)?.[1];
+      if (miniplayerWatchID) return miniplayerWatchID;
+    }
   }
 }
